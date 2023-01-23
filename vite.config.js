46fbb936314a9d706365293becc4b8dcaa3219fa
Yuri Marcon <yurimarcon@MacBook-de-Yuri.local>
2023-01-23 19:30:48 -0300

    Novo teste de adicao de pwa

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-launchericon-144-144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'android-launchericon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-launchericon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
