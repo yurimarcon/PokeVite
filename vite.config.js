e569543950f8bea32693b0c72e5088470e6b3c90
Yuri Marcon <yurimarcon@MacBook-de-Yuri.local>
2023-01-23 20:24:48 -0300

    Adicionando suporte a PWA

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,34 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'PokeVite',
+        short_name: 'PokeVite',
+        description: 'Pokedex com Vue.js + Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
