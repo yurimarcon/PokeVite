0f1d9d686624ad74511fb29a1a258d83e1d26b4c
Yuri Marcon <yurimarcon@MacBook-de-Yuri.local>
2023-01-23 22:25:25 -0300

    Deploy do App

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
